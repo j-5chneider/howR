60bec9249dbe52a2f789c3d00ef9c2d73f61e63c
dein-github-name <meine-email@host.de>
2020-10-30 00:09:17 +0100

    update

--- .binder/install.R
+++ .binder/install.R
@@ -1,4 +1,4 @@
-install.packages(
+install.packages(c(
   "countdown",
   "dplyr",
   "fontawesome",
@@ -11,5 +11,5 @@ install.packages(
   "plotly",
   "rio",
   "rmarkdown",
-  "tidyverse"
+  "tidyverse")
 )
